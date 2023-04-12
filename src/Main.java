50bbe895df4617bd5ee131c350a6668aa5a83313
7thhokagie <7thhokagie@gmail.com>
2023-04-11 19:52:40 -0500

    second tomatela

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("I am a finished project");
+        System.out.println("Man city puta que vergas");
     }
 }
